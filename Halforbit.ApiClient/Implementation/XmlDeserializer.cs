156d75dc8bfaa1266c7b5164c1d7a07d1bd23f25
Ryan O'Shaughnessy <ryano@purecars.com>
2020-01-23 07:15:56 -0500

    Add XML Serialization Option and Extension Methods for XML and Custom Serialization
    Add extension methods to support easily switching to XML or custom request serialization / response deserialization.

---
+++ Halforbit.ApiClient/Implementation/XmlDeserializer.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Halforbit.ApiClient.Implementation
+{
+    public class XmlDeserializer : IDeserializer
+    {
+        public static IDeserializer Instance => new XmlDeserializer();
+
+        public TValue Deserialize<TValue>(Stream stream)
+        {
+            var xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(TValue));
+
+            return (TValue)xmlSerializer.Deserialize(stream);
+        }
+    }
+}
